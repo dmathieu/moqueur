8c7c50b7743ac086415e46b1ac876e77bd13686d
Damien Mathieu <damien.mathieu@lim.eu>
2010-03-22 13:52:14 +0100

    the counter should add an entry with the appropriate url

--- spec/lib/requests_counter_spec.rb
+++ spec/lib/requests_counter_spec.rb
@@ -11,4 +11,9 @@ describe 'Counting the ajax requests' do
       @page.execute_js("jQuery.ajax({url: '/test'})")
     end.should change(@page.execute_js("jQuery.counter"), :count).by(1)
   end
+  
+  it 'should create a call with the appropriate url' do
+    @page.execute_js("jQuery.ajax({url: '/test'})")
+    JSON.parse(@page.execute_js("JSON.stringify(jQuery.counter)")).last['url'].should eql('/test')
+  end
 end
