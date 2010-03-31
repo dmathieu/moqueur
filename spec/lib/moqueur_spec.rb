18731107924c5fe5d3f1c546a086883085b86af7
Damien Mathieu <damien.mathieu@lim.eu>
2010-03-31 12:00:01 +0200

    describe with regex

--- spec/lib/moqueur_spec.rb
+++ spec/lib/moqueur_spec.rb
@@ -17,16 +17,18 @@ describe 'Main specs' do
     JSON.parse(@page.execute_js("JSON.stringify(jQuery.ajax({url: '/hello'}))"))['url'].should eql('file:///hello')
   end
   
-  it 'should mock an ajax with a regexp' do
-    @page.execute_js("jQuery.ajax({
-      url: '/regex/1234',
-      success: function() { return 'Executed Success method with regex'; }
-    })").should eql('Executed Success method with regex')
-  end
-  
-  it 'should give the param in the content when regex' do
-    @page.execute_js("jQuery.ajax({
-      url: '/regex/1234'
-    })").should eql('Request with regex. Param: 1234')
+  describe 'with regex' do
+    it 'should mock an ajax with a regexp' do
+      @page.execute_js("jQuery.ajax({
+        url: '/regex/1234',
+        success: function() { return 'Executed Success method with regex'; }
+      })").should eql('Executed Success method with regex')
+    end
+    
+    it 'should give the param in the content when regex' do
+      @page.execute_js("jQuery.ajax({
+        url: '/regex/1234'
+      })").should eql('Request with regex. Param: 1234')
+    end
   end
 end
